de76a07cd746e4ae4e0e15b2b4fbcccaf33ca64c
sarthakvk <kumar.v.sarthak@gmail.com>
2024-01-15 03:49:59 +0530

    rm manual interventions

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	// hclogadapter "github.com/sarthakvk/hex-app/adapters/hclog"
 	"flag"
 	"fmt"
+	raftadapter "github.com/sarthakvk/hex-app/adapters/raft_adapter"
 	"net"
 	"strconv"
-	"time"
-
-	// "time"
-
-	// "github.com/hashicorp/raft"
-
-	"github.com/hashicorp/raft"
-	hclogadapter "github.com/sarthakvk/hex-app/adapters/hclog"
-	raftadapter "github.com/sarthakvk/hex-app/adapters/raft_adapter"
-	// redisadapters "github.com/sarthakvk/hex-app/adapters/redis"
-	// filehandling "github.com/sarthakvk/hex-app/core/file_handling"
 )
 
 var (
-	myAddr = flag.String("address", "localhost:50051", "TCP host+port for this node")
-	raftId = flag.String("raft_id", "", "Node id used by Raft")
-
-	raftDir       = flag.String("raft_data_dir", "data/", "Raft data dir")
+	myAddr        = flag.String("address", "localhost:50051", "TCP host+port for this node")
+	raftId        = flag.String("raft_id", "", "Node id used by Raft")
 	raftBootstrap = flag.Bool("raft_bootstrap", false, "Whether to bootstrap the Raft cluster")
-	logger        = hclogadapter.GetLogger()
 )
 
 func main() {
@@ -41,20 +27,10 @@ func main() {
 		panic(fmt.Sprintf("failed to parse local address (%q): %v", *myAddr, err))
 	}
 
-	rft, net_transport := raftadapter.NewRaft(*raftId, host, portInt, *raftBootstrap)
-	rft.LeadershipTransfer()
-	rft.Apply()
-	if <-rft.LeaderCh() {
-		out := rft.AddVoter(raft.ServerID("B"), raft.ServerAddress("localhost:8001"), 0, time.Duration(time.Duration.Microseconds(100)))
-		_ = rft.AddVoter(raft.ServerID("C"), raft.ServerAddress("localhost:8002"), 0, time.Duration(time.Duration.Microseconds(100)))
-		if out.Error() != nil {
-			panic(out.Error())
+	_, net_transport := raftadapter.NewRaft(*raftId, host, portInt, *raftBootstrap)
 
-		}
-
-		for {
-			<-net_transport.Consumer()
-		}
+	for {
+		<-net_transport.Consumer()
 	}
 
 }
